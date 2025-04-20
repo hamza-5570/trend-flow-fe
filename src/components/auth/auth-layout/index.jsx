2f9e6bc22a34fffa8d26e6e2e80bca34fdf7d311
hamza-5570 <hafizameerhamza87@gmail.com>
2025-04-21 01:13:09 +0500

    changes

--- src/components/auth/auth-layout/index.jsx
+++ src/components/auth/auth-layout/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function AuthLayout({ children }) {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 lg:h-screen">
+    <div className="grid grid-cols-1 lg:grid-cols-2 lg:h-screen overflow-y-auto h-full">
       <div className="bg-black place-content-center lg:rounded-r-3xl">
         <div className="xl:w-[525px] mx-auto px-5 xl:px-0 pb-5">
           <p className="md:w-[80%] lg:w-full text-xl md:text-3xl font-bold text-white pt-32 md:pt-10 lg:pt-16">
@@ -12,37 +12,38 @@ export default function AuthLayout({ children }) {
 
           <div className="pt-5 md:pt-16">
             <p className="text-lg/relaxed md:text-2xl/relaxed font-medium text-white">
-              AI-Powered Forecasting to Prevent Stockouts & Overstock.
+              AI-Powered Forecasting Build for Small Fashion Brands
             </p>
 
             <p className="text-sm/relaxed md:text-base/relaxed text-white pt-3 md:pt-6">
-              Every inventory mistake costs you. Stockouts mean lost sales.
-              Overstock ties up cash and increases storage costs.
+              Every inventory mistake costs you. Stockouts mean missed sales.
+              Overstock ties up cash and eat into profits.
             </p>
 
             <p className="text-sm/relaxed md:text-base/relaxed text-white pt-5 md:pt-14">
-              TrendFlow keeps your inventory balanced—no more, no less.
+              TrendFlow keeps your inventory balanced with just the right amount
+              of stock
             </p>
 
             <div className="pt-5">
               <p className="text-sm md:text-base text-white">
-                ✅ Maximize revenue by keeping best-sellers in stock.
+                ✅ Keep your best-sellers in stock.
               </p>
 
               <p className="text-sm md:text-base text-white pt-1">
-                ✅ Reduce waste & storage costs by avoiding excess inventory.
+                ✅ Avoid over-ordering and reduce storage
               </p>
               <p className="text-sm md:text-base text-white pt-1">
-                ✅ Stay ahead of demand with AI-driven sales forecasts.
+                ✅ Predict demand with AI-powered sales forecasting.
               </p>
               <p className="text-sm md:text-base text-white pt-1">
-                ✅ Enterprise-level forecasting—at a fraction of the cost.
+                ✅ Enterprise-level forecasting at a small business price.
               </p>
             </div>
 
             <p className="text-xl md:text-[27px] text-white pt-10 md:pt-16">
-              First month 50% off. 7-day full refund. Start optimizing your
-              inventory today!
+              Start your 7-day free trial. only $15 per month after. No credit
+              card required. Start optimizing your inventory today!.
             </p>
           </div>
         </div>
