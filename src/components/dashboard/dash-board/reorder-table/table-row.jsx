90a69805ef8b724ddb69a00721db10930f489442
hamza-5570 <hafizameerhamza87@gmail.com>
2025-05-13 02:58:14 +0500

    changes

--- src/components/dashboard/dash-board/reorder-table/table-row.jsx
+++ src/components/dashboard/dash-board/reorder-table/table-row.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import { MdDelete } from "react-icons/md";
 
 export default function TableRow({ item }) {
-  const [deleteAlert,{isLoading}]=useDeleteAlertMutation()
+  const [deleteAlert, { isLoading }] = useDeleteAlertMutation();
 
   const formatDate = (isoString) => {
     return new Date(isoString)?.toISOString()?.split("T")[0];
@@ -25,8 +25,12 @@ export default function TableRow({ item }) {
       <div className="w-[150px] text-sm text-[#61788A]">
         {item?.weeklyDemand}
       </div>
-       <div className="w-[30px] text-sm text-[#61788A]">
-      {isLoading ? <Loader/> : <MdDelete size={20} onClick={()=>deleteAlert(item?.sku)}/>}
+      <div className="w-[30px] text-sm text-[#61788A]">
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <MdDelete size={20} onClick={() => deleteAlert(item?.sku)} />
+        )}
       </div>
     </div>
   );
