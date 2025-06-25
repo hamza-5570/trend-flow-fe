c28fd23f75fd18e46bd276001f36d185385e1515
Aon Mohammad <aon094944@gmail.com>
2025-06-25 18:43:07 +0500

    preview added

---
+++ src/components/dashboard/recorder-alert/table/table-row.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function TableRow({ item }) {
+  const { SKU, Sales, UnitsSold, SaleDate } = item || {};
+
+  const formatDate = (isoString) => {
+    const date = new Date(isoString);
+
+    if (isNaN(date.getTime())) {
+      return "-";
+    }
+
+    return date.toISOString().split("T")[0];
+  };
+
+  return (
+    <div className="min-w-[1000px] flex items-center justify-between border-b border-[#DBE0E5] p-5">
+      <div className="w-[231px] text-sm text-[#61788A]">
+        {formatDate(SaleDate)}
+      </div>
+
+      <div className="w-[226px] text-sm text-[#61788A]">{SKU}</div>
+
+      <div className="w-[229px] text-sm text-[#61788A]">{UnitsSold}</div>
+      <div className="w-[240px] text-sm text-[#61788A]">${Sales}</div>
+    </div>
+  );
+}
--- src/components/dashboard/recorder-alerts/reorder-form.jsx
+++ src/components/dashboard/recorder-alerts/reorder-form.jsx
@@ -4,15 +4,34 @@ import { Button } from "@/components/ui/button";
 import { useGetInventoryQuery } from "@/lib/services/auth-api";
 import { Field, useFormikContext } from "formik";
 import React, { useState } from "react";
+import TableHeader from "./table/table-header";
+import TableRow from "./table/table-row";
+import { useCreateProductMutation } from "@/lib/services/product-api";
+import * as XLSX from "xlsx";
 
-export default function ReorderForm({ isloading,refetch }) {
+export default function ReorderForm({ isloading, refetch }) {
   const [fileInfo, setFileInfo] = useState(null);
   const { values, setFieldValue } = useFormikContext();
+  const [data, setData] = useState([]);
+
+  const [creatingCSV, { isLoading: isCreatingCSV }] =
+    useCreateProductMutation();
   const handleFileChange = async (e) => {
     if (e?.target?.files && e?.target?.files?.length > 0) {
       setFileInfo(e?.target?.files[0]);
       setFieldValue("file", e?.target?.files[0]);
     }
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(e.target.files[0]);
+    reader.onload = (event) => {
+      const buffer = new Uint8Array(event.target.result);
+      const workbook = XLSX.read(buffer, { type: "array" });
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      const data = XLSX.utils.sheet_to_json(worksheet);
+      setData(data);
+    };
+    console.log("e.target.files[0]", e.target.files[0]);
   };
   const handleDrop = async (e) => {
     e.preventDefault();
@@ -89,6 +108,56 @@ export default function ReorderForm({ isloading,refetch }) {
             />
           </label>
         </div>
+      </div>
+
+      {/* Preview */}
+      <div className="mt-6">
+        <p className="text-[22px] font-bold text-[#121417]">Preview</p>
+
+        <div className="overflow-x-auto border border-[#DBE0E5] rounded-xl mt-5">
+          <div className="max-w-[300px] xl:max-w-full">
+            <TableHeader />
+
+            {isCreatingCSV ? (
+              <div className="h-[300px] flex items-center justify-center">
+                <Loader />
+              </div>
+            ) : data?.length === 0 ? (
+              <p className="h-[300px] flex items-center justify-center text-black text-base text-center">
+                No data uploaded yet
+              </p>
+            ) : (
+              <div className="max-h-[440px] overflow-y-scroll">
+                {data.map((item, index) => (
+                  <TableRow item={item} key={index} />
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+
+        {/* <div className="flex items-center justify-between mt-5">
+                <button
+                  onClick={async () => {
+                    const formData = new FormData();
+                    formData.append("file", files);
+      
+                    try {
+                      await updateSales(formData)
+                        .unwrap()
+                        .then((res) => {
+                          toast.success(res?.message);
+                        });
+                    } catch (error) {
+                      console.error("Error uploading file:", error);
+                    }
+                  }}
+                  className="w-[110px] h-10 ml-auto bg-[#F0F2F5] rounded-xl text-sm text-[#121417] font-bold cursor-pointer flex items-center justify-center"
+                >
+                  {isLoading ? <Loader /> : 
+                  "Submit"}
+                </button>
+              </div> */}
       </div>
       <Button
         type="submit"
